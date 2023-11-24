946c012e2b4f5478a87b9e6201e62a5b2823a97f
Veronika4Donchenko4 <donchenko.veronika96@gmail.com>
2023-11-24 20:04:55 +0100

    adding a function to hide a completed task

---
+++ src/store/redux.js
@@ -0,0 +1,14 @@
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import store from './store';
+import TodoApp from './TodoApp';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <TodoApp />
+  </Provider>,
+  document.getElementById('root')
+);
