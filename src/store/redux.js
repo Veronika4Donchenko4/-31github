7c73daf6146a678bcf93d0ad39e23022259b9428
Veronika4Donchenko4 <donchenko.veronika96@gmail.com>
2023-11-29 23:35:40 +0100

    Added functionality to toggle all tasks

--- src/store/redux.js
+++ src/store/redux.js
@@ -1,14 +1,28 @@
 // index.js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import store from './store';
-import TodoApp from './TodoApp';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import { createStore } from 'redux';
+// import store from './store';
+// import TodoApp from './TodoApp';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <TodoApp />
-  </Provider>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <TodoApp />
+//   </Provider>,
+//   document.getElementById('root')
+// );
+
+const toggleAllTodos = () => ({ type: 'TOGGLE_ALL_TODOS' });
+
+const todoReducer = (state = [], action) => {
+  switch (action.type) {
+    // ... (остальной код)
+    case 'TOGGLE_ALL_TODOS':
+      const allCompleted = state.every(todo => todo.completed);
+      return state.map(todo => ({ ...todo, completed: !allCompleted }));
+    default:
+      return state;
+  }
+};
+
